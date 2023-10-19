5095c8bf1b85cf69537d8022e2d429336f0578db
Dilshad Matkulov <dilshadmatkulov@.gmail.com>
2023-10-19 13:27:28 +0600

    Add state

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,25 @@ import {useState} from 'react';
 
 function App() {
   const [number, setNumber] = useState<number[]>([
-    5, 11, 16, 23, 32
-  ]);
+    5, 11, 16, 23, 32]);
 
   const changeNumbers = () => {
-
-  }
+    const initialArr = [5, 11, 16, 23, 32];
+    const newArr: number[] = [];
+    while (newArr.length < 5) {
+      let randomNumber = Math.floor(Math.random() * 36) + 5;
+      if (initialArr.indexOf(randomNumber) !== -1) {
+        newArr.push(randomNumber);
+      }
+    }
+    console.log(newArr);
+    setNumber([]);
+  };
 
   return (
     <div className="App">
       <div>
-        <button>New numbers</button>
+        <button onClick={changeNumbers}>New numbers</button>
       </div>
       <div className="wrapper">
         <Circle value={number[0]}/>
