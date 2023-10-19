8a537517d08ef23f79117040fa9405ac22aa790b
Dilshad Matkulov <dilshadmatkulov@.gmail.com>
2023-10-19 14:12:35 +0600

    fix setNumber, add new array

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,15 @@ function App() {
     5, 11, 16, 23, 32]);
 
   const changeNumbers = () => {
-    const initialArr = [5, 11, 16, 23, 32];
     const newArr: number[] = [];
-    while (newArr.length < 5) {
-      let randomNumber = Math.floor(Math.random() * 36) + 5;
-      if (initialArr.indexOf(randomNumber) !== -1) {
+    for (let i = 0; newArr.length < 5; i++) {
+      let randomNumber = Math.floor(Math.random() * 32) + 5;
+      if (newArr.indexOf(randomNumber) === -1) {
         newArr.push(randomNumber);
       }
     }
-    console.log(newArr);
-    setNumber([]);
+    newArr.sort((a, b) => a - b);
+    setNumber(newArr);
   };
 
   return (
